dbdc030ec9f2907d2e5234767f3aa02953777815
halotv117 <49052156+halotv117@users.noreply.github.com>
2019-04-14 12:54:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,21 +10,39 @@ client.on('message', message => {
 	console.log(message.content);
 	if(message.content.startsWith('Ayee! You reached level 5,')){
 		let member = message.mentions.members.first();
-		let lvl5 = message.member.guild.roles.find('name', 'level 5');
+		let lvl5 = message.member.guild.roles.find('name', 'Grunt (level 5)');
 		console.log("lvl5 role code ran")
 		member.addRole(lvl5);
 	}
 	if(message.content.startsWith('Ayee! You reached level 10,')){
 		let member = message.mentions.members.first();
-		let lvl10 = message.member.guild.roles.find('name', 'level 10');
+		let lvl10 = message.member.guild.roles.find('name', 'Jackal (level 10)');
 		console.log("lvl10 role code ran")
 		member.addRole(lvl10);
 	}
 	if(message.content.startsWith('Ayee! You reached level 20,')){
 		let member = message.mentions.members.first();
-		let lvl20 = message.member.guild.roles.find('name', 'level 20');
+		let lvl20 = message.member.guild.roles.find('name', 'Marine (level 20)');
 		console.log("lvl20 role code ran")
 		member.addRole(lvl20);
+	}
+	if(message.content.startsWith('Ayee! You reached level 30,')){
+		let member = message.mentions.members.first();
+		let lvl30 = message.member.guild.roles.find('name', 'Brute (level 30)');
+		console.log("lvl30 role code ran")
+		member.addRole(lvl30);
+	}
+	if(message.content.startsWith('Ayee! You reached level 40,')){
+		let member = message.mentions.members.first();
+		let lvl40 = message.member.guild.roles.find('name', 'Elite (level 40)');
+		console.log("lvl40 role code ran")
+		member.addRole(lvl40);
+	}
+	if(message.content.startsWith('Ayee! You reached level 50,')){
+		let member = message.mentions.members.first();
+		let lvl50 = message.member.guild.roles.find('name', 'Spartan 4 (level 50)');
+		console.log("lvl50 role code ran")
+		member.addRole(lvl50);
 	}
 	 
 })
